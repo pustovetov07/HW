5684956ec828542b60bdd61d049418a0c17e13d8
pustovetov07 <80600140+pustovetov07@users.noreply.github.com>
2021-03-31 10:52:15 +0300

    Update HW5-3.py

--- HW5/HW5-3.py
+++ HW5/HW5-3.py
@@ -1,38 +1,11 @@
-def clean_text(text:str):
-    scrap = ['.', ',', '?', '!', ':', ';']
-    splited_text = text.split(' ')
-    for word in splited_text:
-        for lit in word:
-            if lit in scrap:
-                word.replace(lit, '')
-    print(splited_text)
-    return splited_text
-
-def find_biggest(text:list):
-    biggest_word = ''
-    for word in text:
-        if len(word) > len(biggest_word):
-            biggest_word = word
-
-    return  biggest_word
-
-def findCommonword(text:list):
-    common_word = ''
-    for word in text:
-        if text.count(word) > text.count(common_word):
-            common_word = text.pop(text.index(word))
-
-    return common_word
-
-def parse(text:str):
-    clear_text = clean_text(text)
-    biggest_word = find_biggest(clear_text)
-    common_word = findCommonword(clear_text)
-    return (common_word, biggest_word)
-
-def main():
-    text = input()
-    print(*parse(text))
-
-if __name__ == '__main__':
-    main()
+from collections import Counter
+word = input('Введите слово: ')
+counter = Counter(word.split(' '))
+print(counter.most_common(n=2))
+longest = None
+for i in counter.most_common():
+    if not longest:
+        longest = i[0]
+    if len(i[0]) > len(longest):
+        longest = i[0]
+print(longest)
