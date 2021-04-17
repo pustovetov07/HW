4870c8c19a840b2feebd57db47a882ff924df5fe
pustovetov07 <80600140+pustovetov07@users.noreply.github.com>
2021-04-17 15:16:27 +0300

    Update HW6-3.py

--- HW6/HW6-3.py
+++ HW6/HW6-3.py
@@ -1,13 +1,7 @@
-from cmath import sqrt
-def is_prime(number):
-    if number % 2 == 0 and number != 2:
+def is_prime(value):
+    if value == 1:
         return False
-    if number == 0 or number == 1:
-        return False
-    for n in range(3, int(sqrt(number).real) + 1, 2):
-        if number % n == 0:
-            return False
-    return True
-
-n = int(input('Введите число: '))
-print(is_prime(n))
+    d = 2
+    while d * d <= value and value % d != 0:
+        d += 1
+    return d * d > value
