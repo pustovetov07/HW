04c963b77851e9d1aafbbb283d67c6bcc74da2d5
pustovetov07 <80600140+pustovetov07@users.noreply.github.com>
2021-04-30 19:50:03 +0300

    Update HW11.py

--- HW11/HW11.py
+++ HW11/HW11.py
@@ -3,9 +3,13 @@ import uuid
 class Figure:
     def cal_perimetr(self):
         raise NotImplementedError
+        
     def cal_square(self):
         raise NotImplementedError
+        
 class Triangle(Figure):
+    
+    
     def __init__(self, side_a, side_b, side_c):
         assert isinstance(side_a, int), 'Переменная должна быть типа int'
         assert isinstance(side_b, int), 'Переменная должна быть типа int'
@@ -14,33 +18,44 @@ class Triangle(Figure):
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
+        
     def _validate_trinangle(self, side_a, side_b, side_c):
         pass
+    
     def cal_perimetr(self):
         return self.side_a + self.side_b + self.side_c
+    
     def cal_square(self):
         per = self.cal_perimetr() / 2
         return (per * (per - self.side_a) * (per - self.side_b) * (per - self.side_c)) ** 0.5
 class Rectangle(Figure):
+    
+    
     def __init__(self, side_a, side_b):
         assert isinstance(side_a, int), 'Переменная должна быть типа int'
         assert isinstance(side_b, int), 'Переменная должна быть типа int'
         self._validate_rectangle(side_a, side_b)
         self.side_a = side_a
         self.side_b = side_b
+        
     def _validate_rectangle(self, side_a, side_b):
-        # add some validation
-        # raise some error if not valid
         pass
+    
     def cal_perimetr(self):
         return (self.side_a + self.side_b) * 2
+    
     def cal_square(self):
         return self.side_a * self.side_b
+    
 class Square(Figure):
+    
+    
     def __init__(self, side_a):
         assert isinstance(side_a, int), 'Переменная должна быть типа int'
         self.side_a = side_a
+        
     def cal_square(self):
         return self.side_a ** 2
+    
     def cal_perimetr(self):
         return self.side_a * 4
